e2a5ea23cd403f5749d5ec60679fba50730fda02
Pradeep <pradeep@seleniumframework.com>
2019-11-14 19:14:18 -0500

    using ubuntu 18.04 & create .chef with sudo

--- course/01-create-play-area/vagrant/chefserver.bootstrap.sh
+++ course/01-create-play-area/vagrant/chefserver.bootstrap.sh
@@ -4,7 +4,7 @@ wget https://packages.chef.io/files/stable/chef-server/12.18.14/ubuntu/18.04/che
 sudo dpkg -i chef-server-core_12.18.14-1_amd64.deb
 rm chef-server-core_12.18.14-1_amd64.deb
 sudo chef-server-ctl reconfigure
-mkdir .chef
+sudo mkdir .chef
 sudo chef-server-ctl user-create user userfirst userlast user@techsparks.guru 'Welcome123' --filename ~/.chef/user.pem
 
 sudo chef-server-ctl org-create techsparks "Tech Spars Guru" --association_user user --filename ~/.chef/techsparks.pem
