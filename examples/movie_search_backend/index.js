be6aa474e6a53f411503d72f9294625d864ecb3f
parthakonda <partha@aptuz.com>
2019-11-16 21:32:21 +0530

    added backend sample

---
+++ examples/movie_search_backend/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+const imdb = require('imdb-api');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+
+  const response = await imdb.get({ name: query.movie },
+    {
+      apiKey: 'xxxxxx', timeout: 30000
+    });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(response));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
